1366563db97dfc4004c0a489183dd6507e4c5175
baban-afnan <shafiumuhd114@gmail.com>
2025-08-07 17:59:37 +0100

    update bvn modification model

---
+++ app/Models/BVNmodification.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BVNmodification extends Model
+{
+     // Explicit table name
+    protected $table = 'bvn_modification';
+
+    
+    protected $fillable = [
+        'user_id',
+        'reference',
+        'modifcation_field_id',
+        'service_id',
+        'nin',
+        'bvn',
+        'description',
+        'status',
+        'transaction_id',
+        'created_at',
+        'updated_at',
+        'comment',
+        'request_id',
+        'submission_date',
+        
+    ];
+
+    // Define statusHistory as an Eloquent relationship method
+    // public function statusHistory()
+    // {
+    //     return $this->hasMany(StatusHistory::class, 'id');
+    // }
+      
+
+ public function modificationField()
+{
+    return $this->belongsTo(ModificationField::class);
+}
+
+
+}
