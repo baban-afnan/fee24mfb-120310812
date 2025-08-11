8d6f716cf980ef902ba859deb99d9b30de5bd02b
baban-afnan <shafiumuhd114@gmail.com>
2025-08-11 16:28:54 +0100

    nin validation updated

--- resources/views/validation.blade.php
+++ resources/views/validation.blade.php
@@ -110,7 +110,7 @@
                 <thead class="thead-dark">
                     <tr>
                         <th>ID</th>
-                        <th>Reference</th>
+                        <th>Field</th>
                         <th>NIN</th>
                         <th>Status</th>
                         <th>Date Created</th>
@@ -121,7 +121,7 @@
                     @forelse ($enrollments as $enrollment)
                         <tr>
                             <td>{{ $enrollment->id }}</td>
-                            <td>{{ $enrollment->reference }}</td>
+                            <td>{{ $enrollment->modifcation_field_name }}</td>
                             <td>{{ $enrollment->nin }}</td>
                             <td>
                                 @php
